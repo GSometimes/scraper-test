0e981406c3e9d760a7e63d7880ba660a37113c35
Gore Sometimes <gore.auluck@gmail.com>
2022-11-11 22:35:22 -0800

    updated class selectors and got data

--- index.js
+++ index.js
@@ -1,31 +1,54 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-async function getIngredients() {
-  let res = await axios.get(
-    'http://www.allrecipes.com/recipe/266826/air-fryer-potato-wedges/'
-  );
+async function getIngredients(url) {
+  let res = await axios.get(url);
 
   const $ = cheerio.load(res.data);
-  let airFryTaters = {
+  // let recipe = {
+  //   image: '',
+  //   ingredients: [],
+  //   directions: {},
+  // };
+
+  // Ben suggestion
+  let recipe = {
+    //consider adding this
+    //works only if url ENDS WITH / also. As shown in example
+    //splits on /, slices the second to last out, replaces - with space
+    //given url below, it would return 'air fryer potato wedges'
+    name: url.split('/').slice(-2, -1)[0].replace(/-/g, ' '),
     image: '',
     ingredients: [],
     directions: {},
   };
 
-  airFryTaters.image = $('.lazy-image-udf').data('src');
+  recipe.image = $('.universal-image__image').data('src');
 
-  $('.ingredients-item-name').each((i, el) => {
-    airFryTaters.ingredients.push($(el).text());
+  $('.mntl-structured-ingredients__list-item').each((i, el) => {
+    recipe.ingredients.push($(el).text().replace(/\n/g, ''));
     // console.log($(el).text());
   });
 
-  $('.section-body').each((i, el) => {
-    airFryTaters.directions[`Step ${i + 1}`] = $(el).text();
+  $('.comp .mntl-sc-block .mntl-sc-block-html').each((i, el) => {
+    recipe.directions[`Step ${i + 1}`] = $(el).text().replace(/\n/g, '');
     // console.log('directions', $(el).text());
   });
 
-  console.log(airFryTaters);
+  console.log(recipe);
 }
 
-getIngredients();
+getIngredients(
+  'http://www.allrecipes.com/recipe/266826/air-fryer-potato-wedges/'
+);
+
+// let recipe = {
+//   //consider adding this
+//   //works only if url ENDS WITH / also. As shown in example
+//   //splits on /, slices the second to last out, replaces - with space
+//   //given url below, it would return 'air fryer potato wedges'
+//   name: url.split('/').slice(-2, -1)[0].replace(/-/g, ' '),
+//   image: '',
+//   ingredients: [],
+//   directions: {},
+// };
